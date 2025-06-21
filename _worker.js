d2a4301d9d864529023cb467d97dfb65112fc1d0
zznnll-cpu <ZNL18859595153@outlook.com>
2025-06-21 11:42:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,10 +12,17 @@ let timestamp = 4102366036000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-ss://YWVzLTEyOC1nY206RXRIRTBCVllLbUJtRjZXV2hGc3crczgyV0hZYkZraW9JVzlXQS93MDRXTT0%3D@212.192.12.106:48293#%E9%A6%99%E6%B8%AF%E8%87%AA%E5%88%9Bvps%20test%201
-trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:30273?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#%E6%98%A5%E6%B1%9F%E8%8A%B1%E6%9C%88%E5%A4%9C
-trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:31210?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#%E6%B4%97%E5%A4%B4%E5%8F%91
-ss://MjAyMi1ibGFrZTMtYWVzLTI1Ni1nY206RUtjSi9HeGVxVW9XV0puS0JobkFCVnJCbVc5YWtqNHhDNWUzWXE2bm5MND06b3JyMy84UDBEKy8yUGYrSEdxK0JIYXNvWDR5T2I4SDJhMTNjaktienBzZz0%3D@212.192.12.106:33962#znl222
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:30273?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk01
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:31210?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk02
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:28780?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk03
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:33824?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk04
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:36940?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk05
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.ab.he.defehaita.xyz:11210?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk06
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.vb.defehaita.xyz:39009?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk07
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.vb.defehaita.xyz:27364?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk08
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.vb.defehaita.xyz:46585?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk09
+trojan://7452381d-63a7-446c-bdcf-fc74ff451ffa@high.vb.defehaita.xyz:39760?security=tls&sni=douyin.com&allowInsecure=1&type=tcp&headerType=none#hk10
+
 
 
 
